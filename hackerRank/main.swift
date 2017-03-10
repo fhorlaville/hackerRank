e6136d852c12011b7180560be586d3560dce09b3
Tam Software <fh@tamsoftware.info>
2017-03-10 16:36:52 +0000

    Initial Commit

---
+++ hackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hackerRank
+//
+//  Created by Franck Horlaville on 10/03/2017.
+//  Copyright © 2017 TAM Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
